b7a62567d955c9d4a41c7c08b69016ce752fc528
Jie LEBLOND <jleblond@e1r3p17.42.fr>
2019-11-04 12:20:47 +0100

    ex02 done

--- Boot_camp_Python/Day00/ex02/whois.py
+++ Boot_camp_Python/Day00/ex02/whois.py
@@ -1,14 +1,24 @@
 import sys
 
+def odd_or_even(nb):
+    if nb == 0:
+        print("I'm Zero.")
+    elif nb % 2 == 0:
+        print("I'm Even")
+    else:
+        print("I'm Odd.")
+
 def check_arg():
-    if len(sys.argv) == 2:
-        if isinstance(sys.argv[1], int):
-            print(int(sys.argv[1]))
+    if len(sys.argv) == 1:
+        pass
+    elif len(sys.argv) == 2:
+        string = sys.argv[1]
+        if string.isdigit():
+            odd_or_even(int(sys.argv[1]))
         else:
             print("ERROR")
     else:
         print("ERROR")
-
 
 if __name__ == '__main__':
     check_arg()
